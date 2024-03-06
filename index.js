aa2577c66081ae81c5121058a7411651bfe132a9
Mahmoud Samir <mahmodsamir28@gmail.com>
2024-03-06 11:45:43 +0200

    add category router

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const categoryRouter = require('./src/routes/category.routes');
 
 const app = express();
 
-app.use('/category',categoryRouter);
+app.use('/category', categoryRouter);
 
 module.exports = app;
